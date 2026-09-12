ed6bc5463efc0163db2553bea834181d7985bba3
aperezd <aperezd12345@gmail.com>
2026-09-12 07:38:09 -0500

    ejercicios

---
+++ tercera-nota/unidad1/positivosynegativo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class positivosynegativo {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in);
+        int numero = leer.nextInt();
+    if (numero > 0) {
+        System.out.println("El número es positivo.");
+    } else if (numero < 0) {
+        System.out.println("El número es negativo.");
+    } else {
+        System.out.println("El número es cero.");
+    }
+    }
+}
